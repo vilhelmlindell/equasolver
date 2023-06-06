c4292bb78db89ed866aa83270f7993e94bfb2e47
alve9999 <alve.lindell@gmail.com>
2023-06-06 20:04:55 +0200

    finished basic expression tree

--- expression.py
+++ expression.py
@@ -1,10 +1,11 @@
+import expression_parser
 
-class node:
+class Node:
     element = None
     right=None
     left=None
 
-class var(node):
+class Var(Node):
     disallowed_names="*+/%^-"
     numerical = True
 
@@ -21,15 +22,15 @@ class var(node):
             self.element = element
             self.numerical=False
 
-class operator(node):
+class Operator(Node):
     op=None
-    allowed_operators="*+/%^-"
+    allowed_Operators="*+/%^-"
     operations=[lambda a,b : a*b,lambda a,b : a+b,lambda a,b : a/b,lambda a,b : a%b,lambda a,b : a**b,lambda a,b : a-b]
 
     def __init__(self,op,left,right):
         if(left == None or right == None):
-            raise Exception("operators must have a two defined nodes")
-        if(op not in self.allowed_operators):
+            raise Exception("Operators must have a two defined nodes")
+        if(op not in self.allowed_Operators):
             raise Exception("Ilegall operator")
         
         self.left=left
@@ -37,40 +38,49 @@ class operator(node):
         self.op=op
 
     def simplification(self):
-        if(type(self.left)==operator):
+        if(type(self.left)==Operator):
             self.left = self.left.simplification()
-        if(type(self.right)==operator):
+        if(type(self.right)==Operator):
             self.right = self.right.simplification()
 
         if(self.right.numerical and self.left.numerical):
-            return var(self.perform_op())
+            return Var(self.perform_op())
         
         if(self.op=="*"):
             #TODO x^2*x simplification
             if(self.right.element==0 or self.left.element==0):
-                return var(0)
+                return Var(0)
             if(self.right.element==1):
-                return var(self.left.element)
+                return self.left
             if(self.left.element==1):
-                return var(self.right.element)
+                return self.right
             if(self.right.element==self.left.element):
-                return operator("^",self.right.element,2)
+                return Operator("^",self.right.element,Var(2))
             
         if(self.op=="/"):
+            if(self.right.element==0):
+                raise Exception("division by zero encounter")
             if(self.right.element==self.left.element):
-                return var(1)
+                return Var(1)
+            if(self.left.element==0):
+                return Var(0)
+            if(self.left.element==1):
+                return Operator("^",self.right.element,Var(-1))
             
         
         if(self.op=="+"):
             if(self.right.element==self.left.element):
-                return operator("*",self.right.element,2)
+                return Operator("*",self.right.element,Var(2))
             
         if(self.op=="-"):
             if(self.right.element==self.left.element):
-                return var(0)
+                return Var(0)
+        
+        if(self.op=="%"):
+            if(int(self.right.element)!=self.right.element or int(self.right.element)!=self.right.element):
+                raise Exception("modulo by floats not allowed")
             
-
-
+        
 
     def perform_op(self):
         if(self.right.numerical==False or self.left.numerical==False):
@@ -78,10 +88,10 @@ class operator(node):
         return self.operations[self.operations.index(self.op)](self.left.element,self.right.element)
     
 
+
+class Expression:
+    root=None
+    def __init__(self,input):
+        self.root = expression_parser.parse_expression(input)
         
 
-
-
-
-
-class expression:
