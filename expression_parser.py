796374bcfadc78117193d5c69e99fc7ee71026c9
Vilhelm <vilhelm.lindell123@gmail.com>
2023-06-08 15:50:41 +0200

    fixed the parser by reimplementing it in a smarter way

--- expression_parser.py
+++ expression_parser.py
@@ -1,103 +1,74 @@
-#from expression import *
+from expression import Operator
+from expression import Var
 
-OPERATORS = "^*/-+"
+OPERATORS = {
+    '^': 0,
+    '*': 1,
+    '/': 2,
+    '-': 3,
+    '+': 4,
+}
 
-def parse_expression(expr) -> Operator
-    expr = expr.replace(' ', '')
+def parse_expression(string):
+    root_index = 0
+    root_precedence = -1
+    operator = ''
     level = 0
-    operator_indices = [[]] * 5
-    for i in range(len(expr)):
-        char = expr[i]
+    for i in range(len(string)):
+        char = string[i]
 
         if char == '(':
             level += 1
         elif char == ')':
             level -= 1
 
-        if level != 0:
-            continue
+        if char in OPERATORS and level == 0:
+            precedence = OPERATORS[char]
+            if precedence >= root_precedence:
+                root_index = i
+                root_precedence = precedence
+                operator = char
 
-        if char == '^':
-            operator_indices[0].append(i)
-        elif char == '*':
-            operator_indices[1].append(i)
-        elif char == '/':
-            operator_indices[2].append(i)
-        elif char == '-':
-            operator_indices[3].append(i)
-        elif char == '+':
-            operator_indices[4].append(i)
+    print(string)
+    left_operand = string[:root_index]
+    right_operand = string[root_index + 1:]
+    print(left_operand)
+    print(right_operand)
 
-    for operator in range(len(operator_indices)):
-        for i in range(len(operator_indices[operator]), -1):
-            #node = Operator(OPERATORS[i])
-            operands = adjacent_operands(expr, i)
+    if is_var(left_operand, True):
+        left = Var(left_operand)
+    else:
+        if left_operand[0] == '(' and left_operand[-1] == ')':
+            left_operand = left_operand.strip("()")
+        left = parse_expression(left_operand)
 
-            if operands[0][-1] == ')':
-                node.left = parse_expression(operands[0])
-            else:
-                node.left = Var(operands[0])
+    if is_var(right_operand, False):
+        right = Var(right_operand)
+    else:
+        if right_operand[0] == '(' and right_operand[-1] == ')':
+            right_operand = right_operand.strip("()")
+        right = parse_expression(right_operand)
 
-            if operands[1][0] == '(':
-                node.right = parse_expression(operands[1])
-            else:
-                node.right = Var(operands[1])
+    return Operator(operator, left, right)
 
-def adjacent_operands(string, index):
-    sides = split_and_remove(string, index)
-    lhs = ""
-    rhs = ""
-    
-    has_found = False
-    level = 0
+def is_var(string, right_to_left):
+    if string[0] == '(' and string[-1] == ')':
+        return False
 
-    # Extract expression to the left of the operator
-    for i in range(len(sides[0]), 0, -1):
-        char = sides[0][i - 1]
+    if right_to_left:
+        for i in range(len(string), -1):
+            char = string[i]
+            if char in OPERATORS:
+                return False
+    else:
+        for i in range(len(string)):
+            char = string[i]
+            if char in OPERATORS:
+                return False
 
-        if char == ')':
-            level += 1
-            has_found = True
-        elif char == '(':
-            level -= 1
-
-        if level == 0:
-            if has_found:
-                lhs = sides[0][i - 1:]
-                break
-            elif OPERATORS.find(char) != -1:
-                lhs = sides[0][i:]
-                break
-
-    has_found = False
-    level = 0
-
-    # Extract expression to the right of the operator
-    for i in range(len(sides[1])):
-        char = sides[1][i]
-
-        if char == '(':
-            has_found = True
-            level += 1
-        elif char == ')':
-            level -= 1
-
-        if level == 0:
-            if has_found:
-                rhs = sides[1][:i + 1]
-                break
-            elif OPERATORS.find(char) != -1:
-                rhs = sides[1][:i]
-                break
-
-    return lhs, rhs
-
-def find(string, char):
-    return [i for i, ltr in enumerate(string) if ltr == char]
+    return True
 
 def split_and_remove(string, index):
     before = string[:index]
     after = string[index+1:]
     return before, after
-
-print(adjacent_operands("5+323+123*1034-5+3", 9))
